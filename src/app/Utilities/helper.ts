5210665aaf522b4bf1021fd1142352ddd4eb2725
dery-byte <emmanueldery31@gmail.com>
2025-07-23 20:36:42 +0000

    committing new login stuff

--- src/app/Utilities/helper.ts
+++ src/app/Utilities/helper.ts
@@ -1,9 +1,9 @@
 //LOCAL API ACCESS
-let baseUrl = 'http://localhost:8088/api/v1';
+// let baseUrl = 'http://localhost:8088/api/v1';
 
 
 //ONLINE HOSTING
-// let baseUrl = 'https://schoolbackend-vbjh.onrender.com/api/v1';
+let baseUrl = 'https://schoolbackend-vbjh.onrender.com/api/v1';
 // let baseUrl='https://cd68-156-38-117-76.ngrok-free.app/api/v1'
 export default baseUrl;
 
