137890094cd005db8e916900e50b7ac4b96dded2
dery-byte <emmanueldery31@gmail.com>
2026-04-16 20:10:11 +0000

    Initial commitssddd

--- src/app/services/api-configuration.ts
+++ src/app/services/api-configuration.ts
@@ -12,7 +12,7 @@ import { Injectable } from '@angular/core';
 })
 export class ApiConfiguration {
   // rootUrl: string = 'http://localhost:8088/api/v1';
-rootUrl: string = 'https://schoolbackend-j5n9.onrender.com/api/v1/api/v1';
+rootUrl: string = 'https://schoolbackend-j5n9.onrender.com/api/v1';
 
 }
 
